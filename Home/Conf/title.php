669a1075ba290f1981e85d9e91cc7655cb801357
15010222001 <35166379+15010222001@users.noreply.github.com>
2018-02-04 18:09:12 +0800

    Update title.php

--- Home/Conf/title.php
+++ Home/Conf/title.php
@@ -1,1 +1,10 @@
-<?php return array(		'common_title' => array(			'purchase_title' => '采购单管理 > 全部采购单',			'plaatemak_title'	=> '制版管理 > 全部制版信息',			'typeset_title'		=> '排版管理 > 全部排版信息',			'print_title'		=> '印制管理 > 全部印制信息',			'bind_title'		=> '装订管理 > 全部装订信息',		));
+<?php 
+return array(
+		'common_title' => array(
+			'purchase_title' => '采购单管理 > 全部采购单',
+			'plaatemak_title'	=> '制版管理 > 全部制版信息',
+			'typeset_title'		=> '排版管理 > 全部排版信息',
+			'print_title'		=> '印制管理 > 全部印制信息',
+			'bind_title'		=> '装订管理 > 全部装订信息',
+		)
+);
